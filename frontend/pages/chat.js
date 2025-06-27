85647fce6fb9b2efe7705c87f3a7566e18ba5551
Usman Rasool <usmanrasool717@gmail.com>
2025-06-28 01:39:30 +0500

    Clean initial commit

---
+++ frontend/pages/chat.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function Chat() {
+  const [messages, setMessages] = useState([]);
+  return (
+    <div className="p-8">
+      <h2 className="text-2xl">Chat Interface</h2>
+      {/* TODO: Chat UI */}
+    </div>
+  );
+}
